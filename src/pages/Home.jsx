005809b0b693679222adf16aeb7b3de4f64ee6d2
Nasrah-muse <nazronafta143@gmail.com>
2025-05-12 19:51:10 +0300

    chnaged image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ import living from "../assets/living.png";
           className="flex items-center border-2 border-gray-600 rounded-full py-10 px-14
          w-40 h-40 space-y-4 flex-col hover:border-gray-200 transition-all duration-300 cursor-pointer"
         >
-          <img src={chair} alt="" />
+          <img src={living} alt="" />
           <h1>Chair</h1>
         </div>
         <div
